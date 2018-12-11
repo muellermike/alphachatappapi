7e229278cc62f269a1a5355dce09876d4e6c0690
MM <mueller.doublem@gmail.com>
2018-12-11 15:47:35 +0100

    new app.use method for http headers

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler and add headers
-app.use(function(req, res, next) {
+// Add headers
+app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -34,6 +34,13 @@ app.use(function(req, res, next) {
 
   // Request headers you wish to allow
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
