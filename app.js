2713f61af19b728a41e0fe3d0648b620d5132870
MM <mueller.doublem@gmail.com>
2018-12-11 15:35:59 +0100

    rewrite to chat api

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,17 +13,27 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler and add headers
 app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404));
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+var chatHistory = [];
+var nicknames = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ message: 'Team Alpha Server ist ready ;)' });
+});
+
+
+// history
+router.get('/history', function (req, res, next) {
+  res.send(chatHistory);
+});
+
+router.post('/history', function (req, res, next) {
+  var date = new Date();
+
+  console.log(req.body);
+  chatHistory.push({ message: req.body.message, nickname: req.body.nickname, date: date });
+
+  res.json({ message: 'History created!' });
+});
+
+// nicknames
+router.get('/nicknames', function (req, res, next) {
+  res.send(nicknames);
+});
+
+router.get('/nicknames/:id', function (req, res, next) {
+  for (var i = 0; nicknames.length > 0; i++) {
+      if (nicknames[i] && nicknames[i].id === req.params.id) {
+          res.send({ username: nicknames[i].username, id: nicknames[i].id });
+      }
+  }
+});
+
+router.post('/nicknames', function (req, res, next) {
+  console.log(req.body);
+  nicknames.push({ username: req.body.username, id: nicknames.length + 1 });
+
+  res.json({ username: req.body.username });
 });
 
 module.exports = router;
